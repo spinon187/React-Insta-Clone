ffdf181f3265ebf4bc340011e17cb8f29802cd26
spinon187 <spinon187@gmail.com>
2019-01-16 03:46:06 -0600

    sorta functional search bar but not really, filters everything

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,23 +3,38 @@ import './App.css';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer';
 import SearchBar from './components/SearchBar/SearchBar';
-import CommentSection from './components/CommentSection/CommentSection';
+// import CommentSection from './components/CommentSection/CommentSection';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       posts: [],
+      searchEntry: ''
       // comments:[]
     }
   }
 
   componentDidMount(){
     this.setState({posts: dummyData});
-    console.log(this.state.posts);
+    // console.log(this.state.posts);
     // console.log(this.state.comments);
   }
 
+  HandleChanges = e => {
+    this.setState({ [e.target.name]: e.target.value});
+  }
+
+  Search = e => {
+    e.preventDefault();
+    console.log('zoom');
+    this.setState({
+      posts: this.state.posts.filter(
+        post => post.username === e.target.name
+      )
+    })
+    console.log(this.state.posts);
+  }
   // PlusLikes = e => {
   //   console.log('blam');
   //   e.preventDefault();
@@ -70,7 +85,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar
+          posts={this.state.posts}
+          HandleChanges = {this.HandleChanges}
+          Search = {this.Search}
+        />
         <div>
           {this.state.posts.map(post =>{
             return (
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,16 +1,82 @@
 import React from 'react';
+// import React, { Component } from 'react';
 import './SearchBar.css';
 import logo from './img/instacam.png';
 import cursive from './img/instalogo.png';
 
-const SearchBar = () => {
+// class SearchBar extends Component {
+//     constructor(props){
+//         super(props);
+//             this.state = {
+//                 posts: props.posts,
+//                 search: ''
+//             }
+//     }
+
+//     HandleChanges = e => {
+//         this.setState({ [e.target.name]: e.target.value});
+//     };
+
+//     Search = searchString => {
+//         // e.preventDefault();
+//         this.setState({
+//             search: searchString});
+//         this.setState({
+//             posts: this.state.posts.filter(
+//                 post => post.username.includes(this.state.searchString) === true
+//             )
+//         })
+//     }
+
+//     render(){
+//         return (
+//             <div className='search-bar'>
+//                 <div className='branding'>
+//                     <img className='logo' src={logo} alt='Instagram Camera'/>
+//                     <img className='cursive' src={cursive} alt='Cursive Instagram' />
+//                 </div>
+//                 <form className='search-form' onSubmit={this.Search}>
+//                     <input
+//                         value={this.searchString}
+//                         // index={props.index}
+//                         name='text'
+//                         className='search-field'
+//                         type='text'
+//                         placeholder='Search'
+//                         onChange={this.HandleChanges}
+//                     />
+//                 </form>
+//                 <div className='search-icons'>
+//                     <i className="far fa-compass"></i>
+//                     <i className="far fa-heart"></i>
+//                     <i className="far fa-user"></i>
+//                 </div>
+//             </div>
+//         )
+//     };
+// }
+
+
+
+const SearchBar = props => {
     return (
         <div className='search-bar'>
             <div className='branding'>
                 <img className='logo' src={logo} alt='Instagram Camera'/>
                 <img className='cursive' src={cursive} alt='Cursive Instagram' />
             </div>
-            <input type='text' className='search-field' placeholder='Search' />
+            <form className='search-form' onSubmit={props.Search}>{}
+                <input
+                    value={props.username}
+                    // index={props.index}
+                    name='text'
+                    className='search-field'
+                    type='text'
+                    placeholder='Search'
+                    onChange={props.HandleChanges}
+                />
+                <button type = 'submit'>Search</button>
+            </form>
             <div className='search-icons'>
                 <i className="far fa-compass"></i>
                 <i className="far fa-heart"></i>
